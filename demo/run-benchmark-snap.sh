fb695c5c1ef733aa3655f933f0d13c09a0f1278e
Chris Patterson <chris.patterson@canonical.com>
2020-05-19 13:31:57 -0400

    cleanup
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- demo/run-benchmark-snap.sh
+++ demo/run-benchmark-snap.sh
@@ -3,7 +3,7 @@
 set -e
 
 for x in hardware-observe home mount-observe network-bind network-control network-observe raw-usb; do
-  sudo snap connect openvino-core18-example:$x
+  sudo snap connect openvino-examples:$x
 done
 
-openvino-core18-example.benchmark-app -m models/squeezenet1.1.xml -d MYRIAD
+openvino-examples.benchmark-app -m models/squeezenet1.1.xml -d MYRIAD
