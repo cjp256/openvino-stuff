5303dfac30a301ec20245cec0d475efb92934b39
Chris Patterson <chris.patterson@canonical.com>
2020-06-15 14:45:10 -0400

    wip

--- demo/run-benchmark-snap.sh
+++ demo/run-benchmark-snap.sh
@@ -3,7 +3,7 @@
 set -e
 
 for x in hardware-observe home mount-observe network-bind network-control network-observe raw-usb; do
-  sudo snap connect openvino-examples:$x
+  sudo snap connect openvino-samples:$x
 done
 
-openvino-examples.benchmark-app -m squeezenet1.1.xml -d MYRIAD
+openvino-samples.benchmark-app -m squeezenet1.1.xml -d MYRIAD
