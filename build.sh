dcc264d614429bc946203800103dd49f35b755ef
Chris Patterson <chris.patterson@canonical.com>
2020-06-17 21:29:30 +0000

    clean

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 git clone git://github.com/cjp256/snapcraft -b openvino
 
 pushd snapcraft
+snapcraft clean
 snapcraft snap
 sudo snap install *.snap --classic --dangerous
 popd
 
 pushd snaps/openvino-toolkit-unofficial
+snapcraft clean
 snapcraft snap
 sudo snap install *.snap --dangerous
 popd
 
 pushd snaps/openvino-examples
+snapcraft clean
 snapcraft snap
 sudo snap install *.snap --dangerous
 popd
